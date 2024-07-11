17e9d5a488ef02a80eb97c6b19cb4d95e3082651
AAgarwal04 <121470779+AAgarwal04@users.noreply.github.com>
2024-07-11 15:48:21 -0500

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,13 +1,16 @@
 import numpy as np
+import tensorflow as tf
 from pycoral.utils import edgetpu
 from pycoral.adapters import common
 
 # Load the model
-interpreter = edgetpu.make_interpreter('your_model_quantized.tflite')
+interpreter = edgetpu.make_interpreter('environmentModel_quantized.tflite')
 interpreter.allocate_tensors()
 
 # Prepare input data
 input_data = np.array([60, 30504, 101, 26, 48.57692308, 77, 39, -81.71794872, -66], dtype=np.float32)
+input_data = tf.keras.backend.clip(input_data, -128, 127)  # Clip the values to the INT8 range
+input_data = tf.cast(input_data, tf.int8)
 input_data = np.expand_dims(input_data, axis=0)  # Add batch dimension
 
 # Set input tensor
