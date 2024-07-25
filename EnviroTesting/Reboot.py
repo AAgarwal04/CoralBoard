d6efd559cc31783bc780cd66bf3aaa7b7d7b1db9
AAgarwal04 <121470779+AAgarwal04@users.noreply.github.com>
2024-07-25 16:00:57 -0500

    Add files via upload

--- EnviroTesting/Reboot.py
+++ EnviroTesting/Reboot.py
@@ -7,5 +7,12 @@ import subprocess
 def reboot_board():
     subprocess.run(["sudo", "reboot"])
 
-sleep(65*60)
+
+sleep(5)
+thermFile = "/sys/class/thermal/thermal_zone0/trip_point_4_temp"
+file = open(thermFile, "w")
+file.write("25000")
+file.flush()
+file.close()
+sleep(25*60)
 reboot_board()
