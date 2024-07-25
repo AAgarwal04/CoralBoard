905d8924d7b10e30b84e34621c1cb96027d14bdf
AAgarwal04 <121470779+AAgarwal04@users.noreply.github.com>
2024-07-25 15:28:05 -0500

    Add files via upload

--- CoralFiles/realTimeInference.py
+++ CoralFiles/realTimeInference.py
@@ -6,18 +6,22 @@ from luma.core.render import canvas
 from PIL import ImageDraw
 from time import sleep
 import argparse
-import itertools
 import threading
 import os
 import keyboard
 from periphery import GPIO, Serial
-import shutil
 import subprocess
 import asyncio
 import warnings
 from bleak import BleakScanner
+from tflite_runtime.interpreter import Interpreter
+from tflite_runtime.interpreter import load_delegate
+import joblib
 
 DEFAULT_CONFIG_LOCATION = os.path.join(os.path.dirname(__file__), 'cloud_config.ini')
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 def update_display(display, msg):
     with canvas(display) as draw:
@@ -30,109 +34,113 @@ def get_wifi_info():
     output = subprocess.check_output(['nmcli', '-f', 'SIGNAL', 'dev', 'wifi', 'list'], encoding='utf-8')
     output = output.split('\n')[1:]
     output = list(filter(None, output))
-    # output = output[:len(output)-1]
     signal_strengths = []
     for line in output:
         if line[0].isdigit():
             signal_strengths.append(int(line.strip()))
     signal_strengths = list(filter(None, signal_strengths))
-    #print(signal_strengths)
     avg = (sum(signal_strengths)/len(signal_strengths)) if len(signal_strengths) != 0 else 0
-    if (len(signal_strengths) == 0):
+    if len(signal_strengths) == 0:
         return 0, 0, 0
     else:
         return len(signal_strengths), avg, max(signal_strengths)
 
 async def scan_bluetooth():
-    scanner = BleakScanner()
-    devices = await scanner.discover(timeout=3.0)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        scanner = BleakScanner()
+        devices = await scanner.discover(timeout=3.0)
     rssiStrength = []
     for device in devices:
         rssi = device.rssi if hasattr(device, 'rssi') else "Unknown"
         rssiStrength.append(rssi)
     rssiStrength = list(filter(lambda x: x != "Unknown", rssiStrength))
     avg = sum(rssiStrength)/len(rssiStrength) if len(rssiStrength) != 0 else 0
-    if (len(rssiStrength) == 0):
+    if len(rssiStrength) == 0:
         return 0, 0, 0
     else:
         return len(rssiStrength), max(rssiStrength), avg
 
-def main():
+def load_model(model_path):
+    edgetpu_delegate = load_delegate('libedgetpu.so.1')
+    interpreter = Interpreter(
+        model_path=model_path,
+        experimental_delegates=[edgetpu_delegate])
+    interpreter.allocate_tensors()
+    return interpreter
 
-    # Pull arguments from command line.
+def load_scaler(scaler_path):
+    return joblib.load(scaler_path)
+
+def preprocess_data(scaler, X):
+    return scaler.transform(X)
+
+def predict(interpreter, scaler, X):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    X_scaled = preprocess_data(scaler, X)
+    
+    input_scale, input_zero_point = input_details[0]['quantization']
+    row_quantized = X_scaled[0] / input_scale + input_zero_point
+    row_quantized = row_quantized.astype(input_details[0]['dtype'])
+    interpreter.set_tensor(input_details[0]['index'], np.expand_dims(row_quantized, axis=0))
+    
+    interpreter.invoke()
+    
+    output = interpreter.get_tensor(output_details[0]['index'])
+    
+    output_scale, output_zero_point = output_details[0]['quantization']
+    output = (output.astype(np.float32) - output_zero_point) * output_scale
+    
+    return output[0]
+
+def main():
+    warnings.filterwarnings("ignore", category=RuntimeWarning, module="bleak")
+    
     parser = argparse.ArgumentParser(description='Enviro Kit Demo')
-    parser.add_argument('--display_duration',
-            help='Measurement display duration (seconds)', type=int,
-            default=5)
-    parser.add_argument('--upload_delay', help='Cloud upload delay (seconds)',
-            type=int, default=300)
-    parser.add_argument(
-            '--cloud_config', help='Cloud IoT config file', default=DEFAULT_CONFIG_LOCATION)
+    parser.add_argument('--display_duration', help='Measurement display duration (seconds)', type=int, default=5)
+    parser.add_argument('--upload_delay', help='Cloud upload delay (seconds)', type=int, default=300)
+    parser.add_argument('--cloud_config', help='Cloud IoT config file', default=DEFAULT_CONFIG_LOCATION)
     args = parser.parse_args()
-    # Create instances of EnviroKit and Cloud IoT.
+    
     enviro = EnviroBoard()
-    button = GPIO("/dev/gpiochip2", 9, "in")
+
+    model_path = '../environmentModel_quantized_edgetpu.tflite'
+    scaler_path = '../scaler.pkl'
+    interpreter = load_model(model_path)
+    scaler = load_scaler(scaler_path)
 
     while True:
-        # Indefinitely update display and upload to cloud.
         sensors = {}
-        read_period = int(args.upload_delay / (2 * args.display_duration))
-        # flag = True if (input("Inside or Outside (1 or 0): ") == '1') else False
         num = 0
-
         while True:
-            sensors['temperature'] = int(enviro.temperature)
-            sensors['humidity'] = int(enviro.humidity)
-            sensors['ambient_light'] = int(enviro.ambient_light)
-            sensors['pressure'] = int(enviro.pressure)
-            sensors['wifiAmnt'], sensors['wifiAvg'], sensors['wifiMax'] = get_wifi_info()
-            sensors['bleAmnt'], sensors['bleMax'], sensors['bleAvg'] = asyncio.run(scan_bluetooth())
-            warnings.filterwarnings("ignore", category=FutureWarning)
+            try:
+                sensors['humidity'] = int(enviro.humidity)
+                sensors['ambient_light'] = int(enviro.ambient_light)
+                sensors['wifiAmnt'], sensors['wifiAvg'], sensors['wifiMax'] = get_wifi_info()
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", RuntimeWarning)
+                    sensors['bleAmnt'], sensors['bleMax'], sensors['bleAvg'] = asyncio.run(scan_bluetooth())
 
-            # Load model and prepare interpreter
-            interpreter = edgetpu.make_interpreter('enviroModel_edgetpu.tflite')
-            interpreter.allocate_tensors()
+                input_data = np.array([
+                    sensors["humidity"], sensors["ambient_light"],
+                    sensors["wifiAmnt"], sensors["wifiAvg"], sensors["wifiMax"],
+                    sensors['bleAmnt'], sensors["bleAvg"], sensors["bleMax"]
+                ], dtype=np.float32)
 
-            # Prepare input data
-            arr = []
-            input_data = np.array([sensors["humidity"], sensors["ambient_light"], sensors["pressure"], 
-                                   sensors["wifiAmnt"], sensors["wifiAvg"], sensors["wifiMax"], sensors['bleAmnt'], sensors["bleMax"], sensors["bleAvg"]], dtype=np.float32)
-            msg = " ".join(input_data)
-            input_details = interpreter.get_input_details()[0]
-            input_data = input_data.reshape(input_details['shape'])
+                prediction = predict(interpreter, scaler, np.array([input_data]))
+                val = "Inside" if prediction > 0.5 else "Outside"
+                msg = "Count: " + str(num) + " Predict: " + val
+                # print(max(scan_results), sum(scan_results)/len(scan_results))
+                update_display(enviro.display, msg)
+                # print("Inside" if prediction > 0.5 else "Outside")
 
-            # Ensure input data is of type FLOAT32
-            input_data = input_data.astype(np.float32)
+                sleep(5)
+            except Exception as e:
+                msg = "Error"
+                # print(f"An error occurred: {e}")
+                update_display(enviro.display, msg)
+                sleep(5)
 
-            # Run inference
-            interpreter.set_tensor(input_details['index'], input_data)
-            interpreter.invoke()
-
-            # Get output
-            output_data = interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
-            class_id = np.argmax(output_data)
-            score = output_data[0][class_id]
-
-            if class_id == 0:
-                msg = "Outside " + str(num)
-            else:
-                msg = "Inside " + str(num)
-            update_display(enviro.display, msg)
-            sleep(30)
-
-            if button.read() == False:
-                #file1.close()
-                update_display(enviro.display, "Program Terminated :)")
-                # print("Program Terminated")
-                sleep(2)
-                break
-        break
-
-if __name__ == '__main__':
-    sleep(10)
-    thermFile = "/sys/class/thermal/thermal_zone0/trip_point_4_temp"
-    file = open(thermFile, "w")
-    file.write("25000")
-    file.flush()
-    file.close()
+if __name__ == "__main__":
     main()
